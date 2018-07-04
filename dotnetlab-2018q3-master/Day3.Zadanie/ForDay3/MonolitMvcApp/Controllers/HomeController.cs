2ddc8a4dd673c55c3a5ec595a620c877e4ed4df8
Anastasiya Katomakhina <katomakhinaanastasiya@gmail.com>
2018-07-05 02:12:15 +0300

    Entity Framework added.

--- dotnetlab-2018q3-master/Day3.Zadanie/ForDay3/MonolitMvcApp/Controllers/HomeController.cs
+++ dotnetlab-2018q3-master/Day3.Zadanie/ForDay3/MonolitMvcApp/Controllers/HomeController.cs
@@ -3,28 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MonolitMvcApp.Models;
 
 namespace MonolitMvcApp.Controllers
 {
     public class HomeController : Controller
     {
+        MobileContext db = new MobileContext();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Phones.ToList());
         }
 
-        public ActionResult About()
+        public ActionResult MakeOrder(int? id)
         {
-            ViewBag.Message = "Your application description page.";
+            if (id == null)
+                return HttpNotFound();
+            Phone phone = db.Phones.Find(id);
+            if (phone == null)
+                return HttpNotFound();
+            OrderViewModel orderModel = new OrderViewModel { PhoneId = phone.Id };
+            return View(orderModel);
+        }
+        [HttpPost]
+        public ActionResult MakeOrder(OrderViewModel orderModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Phone phone = db.Phones.Find(orderModel.PhoneId);
+                if (phone == null)
+                    return HttpNotFound();
+                decimal sum = phone.Price;
 
-            return View();
+                // если сегодня первое число месяца, тогда скидка в 10%
+                if (DateTime.Now.Day == 1)
+                    sum = sum - sum * 0.1m;
+
+                Order order = new Order
+                {
+                    PhoneId = phone.Id,
+                    PhoneNumber = orderModel.PhoneNumber,
+                    Address = orderModel.Address,
+                    Date = DateTime.Now,
+                    Sum = sum
+                };
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return Content("<h2>Ваш заказ успешно оформлен</h2>");
+            }
+            return View(orderModel);
         }
 
-        public ActionResult Contact()
+        protected override void Dispose(bool disposing)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
---
+++ dotnetlab-2018q3-master/Day3.Zadanie/ForDay3/MonolitMvcApp/Models/MobileContext .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MonolitMvcApp.Models
+{
+    public class MobileContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+
+    public class StoreDbInitializer : DropCreateDatabaseAlways<MobileContext>
+    {
+        protected override void Seed(MobileContext db)
+        {
+            db.Phones.Add(new Phone { Name = "Nokia Lumia 630", Company = "Nokia", Price = 220 });
+            db.Phones.Add(new Phone { Name = "iPhone 6", Company = "Apple", Price = 320 });
+            db.Phones.Add(new Phone { Name = "LG G4", Company = "lG", Price = 260 });
+            db.Phones.Add(new Phone { Name = "Samsung Galaxy S 6", Company = "Samsung", Price = 300 });
+            db.SaveChanges();
+        }
+    }
+}
